3f8d88d047e09666d77299f0b02f7b3ca91393ec
a6iyyu <ffst4irs@gmail.com>
2024-12-13 14:12:07 +0700

    Fix the code in public/index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Http\Kernel;
 
 define('LARAVEL_START', microtime(true));
 
@@ -12,8 +11,3 @@ require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')->handleRequest(Request::capture());
-
-$app = require_once __DIR__ . "/../bootstrap/app.php";
-$kernel = $app->make(Kernel::class);
-$response = $kernel->handle($request = Request::capture())->send();
-$kernel->terminate($request, $response);
