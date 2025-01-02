b72b72a99f0e90f1b31e5dc8c991198c342ceda2
a6iyyu <ffst4irs@gmail.com>
2025-01-02 22:30:57 +0700

    :package: Update packages.

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -54,7 +54,7 @@
     }
   </style>
   @viteReactRefresh
-  @vite(["resources/css/app.css", "resources/js/app.js"])
+  @vite(["resources/js/app.js"])
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 
